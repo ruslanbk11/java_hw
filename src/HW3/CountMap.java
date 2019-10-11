e996333fd2d000deb733092892866d4ac30ea7a4
ruslanbk11 <33201022+ruslanbk11@users.noreply.github.com>
2019-10-11 13:45:38 +0300

    realised some methods

--- src/HW3/CountMap.java
+++ src/HW3/CountMap.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountMap<K> implements CountMapable<K> {
- 
+
     private Map<K, Integer> hashMap;
- 
+
     public CountMap() {
         this.hashMap = new HashMap<>();
     }
- 
+
     @Override
     public void add(K key) {
         if (hashMap.containsKey(key)){
@@ -17,27 +17,52 @@ public class CountMap<K> implements CountMapable<K> {
             hashMap.put(key, 1);
         }
     }
- 
+
     @Override
-    public void addAll(CountMapable countMapable) {
- 
+    public void addAll(CountMap<K> countMap) {
+        for (K k : countMap.hashMap.keySet()) {
+            if (hashMap.containsKey(k)){
+                map.put(k, countMap.getCount(k) + hashMap.get(k));
+            } else {
+                map.put(k, countMap.getCount(k));
+            }
+        }
     }
- 
+
     @Override
-    public int count(K key) {
+    public int getCount(K key) {
         if (hashMap.containsKey(key)){
             return hashMap.get(key);
         }
         return 0;
     }
- 
+
+    @Override
+    public int remove(K key) {
+        if (hashMap.containsKey(key)){
+            return hashMap.remove(key);
+        }
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return hashMap.size();
+    }
+
     @Override
     public Map<K, Integer> asMap(){
         return hashMap;
     }
- 
+
     @Override
-    public void copyTo(Map map) {
- 
+    public void copyTo(Map<K, Integer> map) {
+        for (K k : hashMap.keySet()) {
+            if (map.containsKey(k)){
+                map.put(k, map.get(k) + hashMap.get(k));
+            } else {
+                map.put(k, hashMap.get(k));
+            }
+        }
     }
 }
